fa07d5ed4e54de8ead0a2fc0c08152ff5e914224
suman <naidusuman.b@gmail.com>
2024-12-24 20:58:59 +0530

    ec2 creation

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-2"
+resource "aws_instance" "ec1" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "t2.micro"
 }
